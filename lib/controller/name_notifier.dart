b99f074ca3e18a3c4ad3354b254fe21dace45fcd
Murat Iyigun <miyigun@hotmail.com>
2023-11-09 08:52:36 +0300

    Third commit

---
+++ lib/controller/name_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NameNotifier extends StateNotifier<List<String>>{
+  NameNotifier() : super([]);
+
+  void addNames(String names) {
+    state= [...state, names];
+  }
+
+  void deleteNames(String names) {
+    state= [
+      for (final loopNames in state)
+        if (names!=loopNames) loopNames,
+    ];
+  }
+}
+
+final nameNotifierProvider=StateNotifierProvider((ref){
+  return NameNotifier();
+});
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example4/controller/name_notifier.dart';
 
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final nameStateNotifierProvider=ref.watch(nameNotifierProvider.notifier).state;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Example'),
@@ -14,13 +17,22 @@ class Home extends ConsumerWidget {
         padding: const EdgeInsets.all(18.0),
         child: Center(
           child: ListView.builder(
+            itemCount: nameStateNotifierProvider.length,
               itemBuilder: (context,index){
                 return Text(
-
+                  nameStateNotifierProvider[index].toString(),
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 );
-              }
+              },
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=> ref.read(nameNotifierProvider.notifier).addNames('Now you can add as many names as you want!'),
+          child: Icon(Icons.add),
       ),
     );
   }
